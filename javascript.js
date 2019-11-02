65144c9b60a327f0f1ed71ce87a7ed93f561b899
Brynn Chapman <brynn_m_chapman@outlook.com>
2019-11-02 01:25:23 -0500

    Added documents.

---
+++ javascript.js
@@ -0,0 +1,41 @@
+$(function(){
+    populateButtons(searchArray,'searchButton','#buttonsArea');
+})
+
+var searchArray = ["Dog","Cat", "Bird"];
+
+function populateButtons(searchArray,classToAdd,addArea){
+    $(addArea).empty();
+    for(var i=0; i<searchArray.length; i++){
+        var a = $('<button>');
+        a.addClass(classToAdd);
+        a.attr('dataType',searchArray[i]);
+        a.text(searchArray[i]);
+        $(addArea).append(a);
+    }
+}
+
+$(document).on('click','.searchButton',function(){
+    $('#searches').empty();
+    var type = $(this).data('type');
+    var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+type+'&api_key=ftiUgQL3tHT1bJddfbQnMJNvSdMYG1Ct&limit=10';
+    $.ajax({url:queryURL,method:'GET'})
+        .done(function(response){
+            for(var i=0;i<response.data.length;i++){
+                var searchDiv = $('<div class="searchItem">');
+                var rating = response.data[i].rating;
+                var p = $('<p>').text('Rating: '+rating);
+                var animated = response.data[i].images.fixed_height.url;
+                var still = response.data[i].images.fixed_height_still.url;
+                var image = $('<img>');
+                image.attr('src',still);
+                image.attr('data-still',still);
+                image.attr('data-animated',animated);
+                image.attr('data-state','still');
+                image.addClass('searchImage');
+                searchDiv.append(p);
+                searchDiv.append(image);
+                $('#searches').append(searchDiv);
+
+    }
+})
