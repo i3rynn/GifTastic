2d12be6d9b2075867b85fbb0bfc907921b9ba209
Brynn Chapman <brynn_m_chapman@outlook.com>
2019-11-02 02:21:26 -0500

    Updated code.

--- javascript.js
+++ javascript.js
@@ -36,6 +36,23 @@ $(document).on('click','.searchButton',function(){
                 searchDiv.append(p);
                 searchDiv.append(image);
                 $('#searches').append(searchDiv);
-
+            }
+    })
+})
+$(document).on('click','.searchImage',function(){
+    var state = $(this).attr('data-state');
+    if(state == 'still'){
+        $(this).attr('src',$(this).data('animated'));
+        $(this).attr('data-state', 'animated');
+    }else{
+        $(this).attr('src',$(this).data('still'));
+        $(this).attr('data-state', 'still'); 
     }
+})
+
+$('#addSearch').on('click',function(){
+    var newSearch = $('input').eq(0).val();
+    searchArray.push(newSearch);
+    populateButtons(searchArray,'searchButton','#buttonsArea');
+    return false;
 })
